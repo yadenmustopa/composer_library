eb09818d7b81558a8ceef845f6c40d38c0e5600b
yadenmustopa <brildent@gmail.com>
2022-11-27 08:10:05 +0700

    update contoh

---
+++ src/Customer.php
@@ -0,0 +1,18 @@
+<?php 
+namespace Yaden\ComposerLibrary;
+
+class Customer
+{
+    
+    public function __construct( private string $name )
+    {
+    }
+
+    public function sayHello( string $name ):string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+    
+}
+
+
