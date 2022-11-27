a4d76ea45c2135e4b38957ceabb3e04cf5e24778
yadenmustopa <brildent@gmail.com>
2022-11-27 08:36:59 +0700

    test upgrade

--- src/Customer.php
+++ src/Customer.php
@@ -4,11 +4,11 @@ namespace Yaden\ComposerLibrary;
 class Customer
 {
     
-    public function __construct( private string $name )
+    public function __construct( private string $name  )
     {
     }
 
-    public function sayHello( string $name ):string
+    public function sayHello( string $name="guest" ):string
     {
         return "Hello $name, my name is $this->name";
     }
